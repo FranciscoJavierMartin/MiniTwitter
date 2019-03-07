400f1105296dcca349a27e2f72b7df61fbec8d94
Francisco <javiermartincuenca@gmail.com>
2019-03-07 21:21:06 +0100

    Tweet fragment added

---
+++ app/src/main/java/com/azure/minitwitter/MyTweetRecyclerViewAdapter.java
@@ -0,0 +1,98 @@
+package com.azure.minitwitter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.azure.minitwitter.common.Constants;
+import com.azure.minitwitter.common.SharedPreferencesManager;
+
+import com.azure.minitwitter.retrofit.response.Like;
+import com.azure.minitwitter.retrofit.response.Tweet;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+
+public class MyTweetRecyclerViewAdapter extends RecyclerView.Adapter<MyTweetRecyclerViewAdapter.ViewHolder> {
+
+    private Context ctx;
+    private List<Tweet> mValues;
+    String username;
+
+    public MyTweetRecyclerViewAdapter(Context context, List<Tweet> items) {
+        mValues = items;
+        ctx = context;
+        username = SharedPreferencesManager.getSomeStringValue(Constants.PREF_USERNAME);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+
+        holder.tvUsername.setText(holder.mItem.getUser().getUsername());
+        holder.tvMessage.setText(holder.mItem.getMensaje());
+        holder.tvLikesCount.setText(holder.mItem.getLikes().size());
+
+        String photoUrl = holder.mItem.getUser().getPhotoUrl();
+
+        if(!holder.mItem.getUser().getPhotoUrl().equals("")){
+            Glide.with(ctx)
+                    .load("https//www.minitwitter.com/apiv1/uploads/photos/"+photoUrl)
+                    .into(holder.ivAvatar);
+        }
+
+        for(Like like: holder.mItem.getLikes()){
+            if(like.getUsername().equals(username)){
+                Glide.with(ctx)
+                        .load(R.drawable.ic_like_pink)
+                        .into(holder.ivLike);
+                holder.tvLikesCount.setTextColor(ctx.getResources().getColor(R.color.pink));
+                holder.tvLikesCount.setTypeface(null, Typeface.BOLD);
+            }
+            break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView ivAvatar;
+        public final ImageView ivLike;
+        public final TextView tvUsername;
+        public final TextView tvMessage;
+        public final TextView tvLikesCount;
+        public Tweet mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            ivAvatar = view.findViewById(R.id.imageViewAvatar);
+            ivLike = view.findViewById(R.id.imageViewLike);
+            tvUsername = view.findViewById(R.id.textViewUsername);
+            tvMessage = view.findViewById(R.id.textViewMessage);
+            tvLikesCount = view.findViewById(R.id.textViewLiesCount);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + tvUsername.getText() + "'";
+        }
+    }
+}
---
+++ app/src/main/java/com/azure/minitwitter/retrofit/response/Like.java
@@ -0,0 +1,103 @@
+
+package com.azure.minitwitter.retrofit.response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Like {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("website")
+    @Expose
+    private String website;
+    @SerializedName("photoUrl")
+    @Expose
+    private String photoUrl;
+    @SerializedName("created")
+    @Expose
+    private String created;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Like() {
+    }
+
+    /**
+     *
+     * @param id
+     * @param username
+     * @param created
+     * @param website
+     * @param description
+     * @param photoUrl
+     */
+    public Like(Integer id, String username, String description, String website, String photoUrl, String created) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.website = website;
+        this.photoUrl = photoUrl;
+        this.created = created;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+
+}
